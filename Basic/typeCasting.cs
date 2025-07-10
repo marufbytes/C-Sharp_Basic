eded7239aeb65ce6ddbc562742516287c18360f6
Maruf Ahammed <marufahammed215@gmail.com>
2025-07-10 08:52:52 +0600

    Create typeCasting.cs

---
+++ Basic/typeCasting.cs
@@ -0,0 +1,19 @@
+using System;
+class Test
+{
+    public static void Main(string[] args)
+    {
+        double salary = 4543.54;
+        Console.WriteLine(salary);
+
+        int salary2 = (int)salary;
+        Console.WriteLine(salary2);
+    }
+ }
+
+ /* Implicit - Automatic conversion
+ int -> double
+
+ Explicit - Manual conversion
+ double->int
+ */
