cd46eca3a7b478b875d261cfd2150354b64de018
Maruf Ahammed <marufahammed215@gmail.com>
2025-07-10 08:31:45 +0600

    Create Variable.cs

---
+++ Basic/Variable.cs
@@ -0,0 +1,23 @@
+using System;
+class Test
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        name = "Maruf Ahammed";
+        int age = 21;
+        double cgpa = 3.11;
+        bool isRegistered = true;
+        char bloodGroup = 'O';
+
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(cgpa);
+        Console.WriteLine(isRegistered);
+        Console.WriteLine(bloodGroup);
+
+        name = "Ahammed Maruf";
+        Console.WriteLine(name);
+
+    }
+ }
